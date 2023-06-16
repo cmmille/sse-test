b9149aeb16906916fc69c256b8f6b34f4e589cfb
cmmille <cmmiller.bio@gmail.com>
2023-06-16 12:31:32 -0700

    Add main

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get "/", to: "main#index"
   # root "articles#index"
   get "/sse-demo", to: "sse_demo#index"
   get "/sse", to: "sse#index"
